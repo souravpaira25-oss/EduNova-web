07eb1f3600b12fad1b781f8275589a9a64c39b09
Sourav Paira <souravpaira25@gmail.com>
2026-06-10 23:15:05 +0530

    firebase debug

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,5 +1,10 @@
 const admin = require("firebase-admin");
 
+console.log("firebase-admin:", admin);
+console.log("PROJECT_ID:", process.env.FIREBASE_PROJECT_ID);
+console.log("CLIENT_EMAIL:", process.env.FIREBASE_CLIENT_EMAIL);
+console.log("PRIVATE_KEY:", process.env.FIREBASE_PRIVATE_KEY ? "FOUND" : "MISSING");
+
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
@@ -8,4 +13,4 @@ admin.initializeApp({
   }),
 });
 
-module.exports = adminw
+module.exports = admin;
