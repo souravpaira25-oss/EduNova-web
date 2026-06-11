5ba26d4e101c5ef0f0733e6ef128981b650060a0
Sourav Paira <souravpaira25@gmail.com>
2026-06-11 17:42:43 +0530

    add all users api

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -275,4 +275,18 @@ router.post("/send-notification", async (req, res) => {
     });
   }
 });
+
+// show all users (for admin)
+router.get("/all-users", async (req, res) => {
+  try {
+    const users = await User.find()
+      .select("-password -verificationToken -resetPasswordToken");
+
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
 module.exports = router;
